3a425f0b829ae2d3ebd0a2b3c8bd6f836313095c
Melissa Perez <melissaperezg.96@gmail.com>
2020-04-11 05:20:31 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,23 +1,23 @@
 class Person
+  
   attr_accessor :partner, :name
+  
   def initialize(name)
     @name = name
   end
+  
   def get_married(person)
     self.partner = person
     if person.class != Person
       begin
         raise PartnerError
       rescue PartnerError => error
-      puts 
-      error.message
+      puts error.message
     end
     else
     person.partner = self
   end
 end
-
-
 
 class PartnerError < StandardError
   def message 
